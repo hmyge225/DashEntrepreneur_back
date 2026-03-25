cdfa74005d8ecbc28424d30f9311c6de4b19e5de
serge Mangoua <jmangoua@edane.org>
2026-03-25 13:01:20 +0000

    Add breeze

---
+++ app/Models/Mentor.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mentor extends Model
+{
+    /** @use HasFactory<\Database\Factories\MentorFactory> */
+    use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
